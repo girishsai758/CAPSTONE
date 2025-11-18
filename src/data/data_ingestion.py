417376705a5d668b99aa0322aaca3edcfb9d8c1c
Girish <dmgirishsai1998@gmail.com>
2025-11-18 05:55:16 +0530

    CI PIPELINE2

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -79,9 +79,8 @@ def main():
         test_size = params['data_ingestion']['test_size']
         # test_size = 0.2
         
-       # df = load_data(data_url='https://raw.githubusercontent.com/vikashishere/Datasets/refs/heads/main/data.csv')
-        s3 = s3_connection.s3_operations("girish12345678990", "AKIAWS34RZ7SGQ57QPMY", "WE/iGEq/5oVphzeIyZJZVDRjTd3G1evLX0vy2qUx")
-        df = s3.fetch_file_from_s3("data.csv")
+        df = load_data(data_url='https://raw.githubusercontent.com/vikashishere/Datasets/refs/heads/main/data.csv')
+       
 
 
 
